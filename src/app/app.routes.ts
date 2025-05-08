0cff879f9f7e5e3481aec79e2579a78bbbc09291
IvanNicolasVio <vioivan@hotmail.com>
2025-05-08 18:30:57 -0300

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'home',
+    },
+    {
+        path:'login',
+        component: LoginComponent
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'aboutUs',
+        component: AboutUsComponent
+    },
+    {
+        path:'**',
+        redirectTo:'home',
+    }
+];
